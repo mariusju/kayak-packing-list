279c1e95ecb8f1ebaf6f2f402b08bfbce0abb68c
Marius Jurgelenas <mariusj@wix.com>
2020-07-02 17:50:28 +0300

    Updates

---
+++ precache-manifest.d9452cbbc41fd8cb4870fa6f5bd05a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7a57e8b1ebe9a77e412af2020a767d",
+    "url": "/kayak-packing-list/index.html"
+  },
+  {
+    "revision": "9108925e610524374e15",
+    "url": "/kayak-packing-list/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6921395f216f6d6fbcd6",
+    "url": "/kayak-packing-list/static/js/2.82fcef9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kayak-packing-list/static/js/2.82fcef9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9108925e610524374e15",
+    "url": "/kayak-packing-list/static/js/main.13fb13da.chunk.js"
+  },
+  {
+    "revision": "ff78c4ed6ed06e61847e",
+    "url": "/kayak-packing-list/static/js/runtime-main.5bf756af.js"
+  }
+]);
